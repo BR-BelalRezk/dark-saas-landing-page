7e876adede2a0ff041b0041e6a1d27cb347af4d6
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-08-10 07:28:06 +0300

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,10 +2,10 @@ import { footer } from "@/constants";
 
 export default function Footer() {
   return (
-    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20 ">
       <div className="container">
         <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-          <pre className="text-center">
+          <pre className="text-center text-sm md:text-base">
             @ 2024 Your Company, Inc. All rights reserved
           </pre>
           <ul className="flex items-center justify-center gap-2.5">
