e3f84a70e86b7f2854fb731af77730d36120d076
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-08-10 07:11:37 +0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import Navbar from "./Navbar";
 export default function Header() {
   return (
     <header className="bg-black w-full">
-      <nav className="py-4 container mx-auto px-5">
+      <nav className="py-5 md:pt-10 container mx-auto px-5">
         <div className="flex  items-center justify-between w-full">
           <figure className="relative">
             <div className=" absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md" />
